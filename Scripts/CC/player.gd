d00d44a1aee2823dd2c3a39eb28c2bdb33d34519
Crawnuts <crawman.mail@gmail.com>
2026-06-04 14:09:46 +0200

    Added torch swing

--- Scripts/CC/player.gd
+++ Scripts/CC/player.gd
@@ -3,17 +3,43 @@ extends CharacterBody2D
 @export var components: Dictionary[String,BaseComponent]
 @export var speed: float = 400
 @export var audio_manager: PAManager
+@export var swing_cd: float = 2
+@export var torch_area: Area2D
+var can_swing: bool = true
+signal swing
 
-#Movement
+func _ready():
+	swing.connect(torch_swing)
+	
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
 func get_input():
 	look_at(get_global_mouse_position())
 	var input_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
-	
+	if Input.is_action_just_pressed("swing") and can_swing == true:
+		swing.emit()
+		can_swing = false
+		swing_cooldown()
+
+func swing_cooldown():
+	await get_tree().create_timer(swing_cd).timeout
+	print("You may Swing")
+	can_swing = true
+
+func torch_swing():
+	var dir = (get_global_mouse_position() - global_position).normalized()
+	torch_area.global_rotation = global_rotation
+	torch_area.monitoring = true
+	torch_area.visible = true
+	await get_tree().create_timer(.25).timeout
+	torch_area.monitoring = false
+	torch_area.visible = false
+
+#Audio
 func _on_ambiance_timer_timeout() -> void: 
 	if audio_manager != null: 
 		audio_manager.play_ambiance_sound()
--- Scripts/torch_scare.gd
+++ Scripts/torch_scare.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+
+func _ready() -> void:
+	monitoring = false
+	visible = false
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
-		queue_free()
+		body.queue_free()
