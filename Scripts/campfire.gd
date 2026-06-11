66aa98cd68a49de3bf97b481242840a030a485cb
Mattyijs <matthijsjanssen2007@gmail.com>
2026-06-11 12:24:46 +0200

    New campfire audio configuration

--- Scripts/campfire.gd
+++ Scripts/campfire.gd
@@ -3,6 +3,7 @@ class_name Campfire
 
 @export var is_extractfire: bool = false
 @export var animated_sprite: AnimatedSprite2D
+@onready var song_player = $CampfireSongPlayer
 
 signal canextract
 
@@ -10,3 +11,11 @@ func _ready() -> void:
 	if is_extractfire:
 		animated_sprite.play("extraction")
 		canextract.emit()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		song_player.play()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		song_player.stop()
