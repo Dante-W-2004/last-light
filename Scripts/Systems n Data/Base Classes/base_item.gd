dbc876beed68a12710267877b9f12a0795f0d69b
Danté W <dantew2004@gmail.com>
2026-06-19 13:01:20 +0200

    Inventory Display - Stable
    Implemented basic inventory UI.
    Implemented `BaseItemData` for improved item handling.
    Implemented `BaseItem` reconstruction from `BaseItemData`.
    Implemented item usage.
    Implemented item dropping.
    Added key binds for picking up items, and toggling inventory UI.
    
    Reorganised folder structure.

---
+++ Scripts/Systems n Data/Base Classes/base_item.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name BaseItem
+
+@export var item_data: BaseItemData
+
+func _ready() -> void:
+	self.add_to_group("Item")
+	item_data.item_scene = load(scene_file_path)
+
+func pick_up(_inventory: InventoryManager) -> void:
+	var data: BaseItemData = item_data.duplicate(true)
+	_inventory.add_to_inventory(data)
+	self.queue_free()
+
+func use(_affected: Node) -> void:
+	for child in get_children():
+		if child is BaseAttribute:
+			child.modify(_affected)
