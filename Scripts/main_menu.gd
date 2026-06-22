c29f9667967b8fa046a485e58c67cae72d6cbc4a
Crawnuts <crawman.mail@gmail.com>
2026-06-22 14:36:34 +0200

    added controls menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,3 +4,8 @@ extends Control
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file(playScene)
+
+
+
+func _on_controls_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/control_menu.tscn")
