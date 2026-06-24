a411956aa74581d0907f07571a2b8d38c4a88009
a-type-of-username <577848@student.fontys.nl>
2026-06-24 12:52:58 +0200

    Added other things to map

--- Procedural Generation/Scripts/map_3.gd
+++ Procedural Generation/Scripts/map_3.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @export var AllTreesIThink : Array[PackedScene] = []
 @onready var trees: Node2D = %TREES
+@onready var campfires: Node2D = %Campfires
+@onready var sentries: Node2D = %Sentries
+@onready var spawner: Node2D = %Spawner
+@onready var items: Node2D = %Items
 
 @export var noise_height_text : NoiseTexture2D
 @export var noise_treesnstuff_text : NoiseTexture2D
@@ -80,9 +84,9 @@ func generate_world():
 				elif noise_value_2 >= 0.7:
 					tall_tiles_arr.append(Vector2i(x,y))
 					
-				if noise_value_2 > 0.3 and noise_value_2 < 0.7 and treesnstuff_value > 0.5 and treesnstuff_value < 0.85:
+				if noise_value_2 > 0.3 and noise_value_2 < 0.7 and treesnstuff_value > 0.5 and treesnstuff_value < 0.7:
 					tile_map.set_cell(trees_n_stuff_layer, Vector2(x,y), source_id, treesnstuff_atlas)
-					for i in range(2):
+					for i in range(1):
 						var TreesPlease: Array[Vector2i] = tile_map.get_used_cells(trees_n_stuff_layer)
 						var RandomCoord: Vector2i = TreesPlease.pick_random()
 						var CoordPosition: Vector2 = tile_map.map_to_local(RandomCoord)
@@ -90,7 +94,7 @@ func generate_world():
 						AddTrees.global_position = CoordPosition
 						TreesPlease.erase(RandomCoord)
 						trees.add_child(AddTrees)
-					for i in range(15):
+					for i in range(1):
 						var TreesPlease: Array[Vector2i] = tile_map.get_used_cells(trees_n_stuff_layer)
 						var RandomCoord: Vector2i = TreesPlease.pick_random()
 						var CoordPosition: Vector2 = tile_map.map_to_local(RandomCoord)
@@ -99,7 +103,40 @@ func generate_world():
 						AddCampfire.global_position = CoordPosition
 						TreesPlease.erase(RandomCoord)
 						placeholderCampfire.erase(AddCampfire)
-						
+				if noise_value_2 > 0.3 and noise_value_2 < 0.7 and treesnstuff_value > 0.3 and treesnstuff_value < 0.5:
+					tile_map.set_cell(trees_n_stuff_layer, Vector2(x,y), source_id, treesnstuff_atlas)
+					for i in range(1):
+						var TreesPlease: Array[Vector2i] = tile_map.get_used_cells(trees_n_stuff_layer)
+						var RandomCoord: Vector2i = TreesPlease.pick_random()
+						var CoordPosition: Vector2 = tile_map.map_to_local(RandomCoord)
+						var SpawningSentriesArray = [$Sentries/Scarecrow, $Sentries/Scarecrow2, $Sentries/Scarecrow3, $Sentries/Scarecrow4, $Sentries/Scarecrow5, $Sentries/Scarecrow6, $Sentries/Scarecrow7, $Sentries/Scarecrow8, $Sentries/Scarecrow9, $Sentries/Scarecrow10, $Sentries/Scarecrow11, $Sentries/Scarecrow12, $Sentries/Scarecrow13, $Sentries/Scarecrow14, $Sentries/Scarecrow15, $Sentries/Scarecrow16, $Sentries/Scarecrow17, $Sentries/Scarecrow18, $Sentries/Scarecrow19, $Sentries/Scarecrow20]
+						var AddSentries: Node2D = SpawningSentriesArray.pick_random()
+						AddSentries.global_position = CoordPosition
+						TreesPlease.erase(RandomCoord)
+						SpawningSentriesArray.erase(AddSentries)
+				if noise_value_2 > 0.3 and noise_value_2 < 0.7 and treesnstuff_value > 0.0 and treesnstuff_value < 0.3:
+					tile_map.set_cell(trees_n_stuff_layer, Vector2(x,y), source_id, treesnstuff_atlas)
+					for i in range(1):
+						var TreesPlease: Array[Vector2i] = tile_map.get_used_cells(trees_n_stuff_layer)
+						var RandomCoord: Vector2i = TreesPlease.pick_random()
+						var CoordPosition: Vector2 = tile_map.map_to_local(RandomCoord)
+						var SpawnerSpawner = [$Spawner/SpawnerTest, $Spawner/SpawnerTest2, $Spawner/SpawnerTest3, $Spawner/SpawnerTest4, $Spawner/SpawnerTest5, $Spawner/SpawnerTest6, $Spawner/SpawnerTest7, $Spawner/SpawnerTest8, $Spawner/SpawnerTest9, $Spawner/SpawnerTest10, $Spawner/SpawnerTest11, $Spawner/SpawnerTest12, $Spawner/SpawnerTest13, $Spawner/SpawnerTest14, $Spawner/SpawnerTest15, $Spawner/SpawnerTest16, $Spawner/SpawnerTest17, $Spawner/SpawnerTest18, $Spawner/SpawnerTest19, $Spawner/SpawnerTest20, $Spawner/SpawnerTest21, $Spawner/SpawnerTest22, $Spawner/SpawnerTest23, $Spawner/SpawnerTest24, $Spawner/SpawnerTest25]
+						var AddSpawner: Node2D = SpawnerSpawner.pick_random()
+						AddSpawner.global_position = CoordPosition
+						TreesPlease.erase(RandomCoord)
+						SpawnerSpawner.erase(AddSpawner)
+				if noise_value_2 > 0.3 and noise_value_2 < 0.7 and treesnstuff_value > 0.0 and treesnstuff_value < 0.3:
+					tile_map.set_cell(trees_n_stuff_layer, Vector2(x,y), source_id, treesnstuff_atlas)
+					for i in range(1):
+						var TreesPlease: Array[Vector2i] = tile_map.get_used_cells(trees_n_stuff_layer)
+						var RandomCoord: Vector2i = TreesPlease.pick_random()
+						var CoordPosition: Vector2 = tile_map.map_to_local(RandomCoord)
+						var ITEMSSPAWN = [$Items/ItemApple, $Items/ItemLog, $Items/ItemApple2, $Items/ItemLog2, $Items/ItemApple3, $Items/ItemLog3, $Items/ItemApple4, $Items/ItemLog4, $Items/ItemApple5, $Items/ItemLog5, $Items/ItemApple6, $Items/ItemLog6, $Items/ItemEvilApple6, $Items/ItemApple7, $Items/ItemLog7, $Items/ItemEvilApple7, $Items/ItemApple8, $Items/ItemLog8, $Items/ItemEvilApple8, $Items/ItemApple9, $Items/ItemLog9, $Items/ItemEvilApple9, $Items/ItemApple10, $Items/ItemLog10, $Items/ItemEvilApple10, $Items/ItemApple11, $Items/ItemLog11, $Items/ItemEvilApple11]
+						var AddItem: Node2D = ITEMSSPAWN.pick_random()
+						AddItem.global_position = CoordPosition
+						TreesPlease.erase(RandomCoord)
+						ITEMSSPAWN.erase(AddItem)
+			
 	tile_map.set_cells_terrain_connect(grass_layer, grass_tiles_arr, 0, terrain_grass_int, false)
 	tile_map.set_cells_terrain_connect(tall_layer, tall_tiles_arr, 0, terrain_tall_int, false)
 	tile_map.set_cells_terrain_connect(water_layer, water_tiles_arr, 0, terrain_water_int, false)
